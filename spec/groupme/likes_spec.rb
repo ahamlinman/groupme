c0b797f04280cd817f472d75e00ace570ef8cc5c
Alex Hamlin <alex@alexhamlin.co>
2016-07-20 21:46:24 -0700

    Fix exception raised when liking messages
    When liking and unliking messages, the body of the GroupMe response
    contains a 'meta' field with a status code and a 'response' field that
    evaluates to nil. Because the body was non-empty, we would return a nil
    response to users of the 'request' method when they expected the full
    response from Faraday. In particular, this led to an exception being
    thrown when liking messages (although messages would still be liked
    successfully).
    
    This fix should allow create_like and destroy_like to work properly.

--- spec/groupme/likes_spec.rb
+++ spec/groupme/likes_spec.rb
@@ -6,10 +6,25 @@ describe GroupMe::Likes do
     @client = GroupMe::Client.new(:token => 'TEST')
   end
 
+  let :data do
+    {
+      :status => 200,
+      :body => {
+        :response => nil,
+        :meta => {
+          :code => 200
+        }
+      }.to_json,
+      :headers => {
+        :content_type => 'application/json; charset=utf-8'
+      }
+    }
+  end
+
   describe '.create_like' do
 
     it 'likes a message' do
-      stub_post('/messages/3/5/like').to_return(:status => 200)
+      stub_post('/messages/3/5/like').to_return(data)
       response = @client.create_like(3, 5)
       expect(response).to eq(true)
     end
@@ -19,7 +34,7 @@ describe GroupMe::Likes do
   describe '.destroy_like' do
 
     it 'likes a message' do
-      stub_post('/messages/234/8/unlike').to_return(:status => 200)
+      stub_post('/messages/234/8/unlike').to_return(data)
       response = @client.destroy_like(234, 8)
       expect(response).to eq(true)
     end
